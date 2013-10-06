4df6961ebc979c7036b427ddde7f32f3eb530dee
Ian OCONNOR <ianoconnor@Ians-MacBook-Air.local>
2013-10-06 12:53:02 +0800

    na

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,75 @@
-'use strict';
+function changeSquareContent(location, className) {
+	document.getElementById('cell' + location).classList.add(className); 
+	document.getElementById('cell' + location).innerHTML = className;
+};
 
-angular.module('TickeyApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+var currentPlayer = "x";
+
+function makeNextMoveAt(location) {
+  markSquareAsOccupiedAt(location);
+  swapCurrentPlayerToOpponent();
+};
+
+function markSquareAsOccupiedAt(location) {
+  if (currentPlayer == "x") {
+  	changeSquareContent(location, "x");
+  } else {
+  	changeSquareContent(location, "o");
+  }
+}
+
+function swapCurrentPlayerToOpponent() {
+  if (currentPlayer == "x") {
+  	currentPlayer = "o";
+  } else {
+  	currentPlayer = "x";
+  }	
+}
+
+
+function handleClick(location) {
+	if (currentSquareClickedAlready(location)) {
+		// do nothing
+	} else {
+		makeNextMoveAt(location)
+	}
+}
+
+// Homework 1
+// this function need to return true or false
+function currentSquareClickedAlready(location) {
+  // TODO
+  // check if the square at location has been occupied
+  // 
+  // ... your code here
+  // ... your code here
+}
+
+// Homework 2
+// this function need to return true or false
+function isTopHorizontalThreeOccupiedByMe() {
+  // TODO
+  // check if the top three square is occupied by X
+  // 
+  // ... your code here
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
